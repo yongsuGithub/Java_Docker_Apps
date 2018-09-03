e4f35373d0d74072d4d036abbf766c0de1a87a5e
yongsu <yongsumaid@outlook.com>
2018-09-03 15:19:31 +0530

    fd
    fdsaf

--- src/main/java/com/yash/em/EmployeeController.java
+++ src/main/java/com/yash/em/EmployeeController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@CrossOrigin(origins = "http://13.232.219.107:8082", maxAge = 3600)
+//d@CrossOrigin(origins = "http://13.232.219.107:8082", maxAge = 3600)
 @RestController
 @RequestMapping("/api")
 public class EmployeeController {
